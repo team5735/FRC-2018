eb4257244e4ee3238399b7126b609c4dca9a05fe
Mingle Li <limingle5@gmail.com>
2018-03-17 14:30:38 -0400

    Revert "removed idea folder from git, made more trajectories for reverse path instead of using reverse method"
    This reverts commit 1ec4d372540920b75d7350b1d3867d888e6a6898.

--- src/frc/team5735/constants/TrajectoryConstants.java
+++ src/frc/team5735/constants/TrajectoryConstants.java
@@ -4,16 +4,16 @@ import frc.team5735.controllers.motionprofiling.Trajectory;
 
 public class TrajectoryConstants {
     public static Trajectory
-        centerToLeftSwitch = new Trajectory("centerToLeftSwitch", false),
-        centerToRightSwitch = new Trajectory("centerToRightSwitch", false),
-        switchBackupToCube = new Trajectory("rightBackupToCube", false),
-        switchBackupToCubeReversed = new Trajectory("rightBackupToCube", true),
+        centerToLeftSwitch = new Trajectory("centerToLeftSwitch"),
+        centerToRightSwitch = new Trajectory("centerToRightSwitch"),
+        switchBackupToCubeReversed = new Trajectory("rightBackupToCube",true),
+            switchBackupToCube = new Trajectory("rightBackupToCube",false),//mirrorable
+//        switchBackupToCube = new Trajectory("centerToRightSwitch"), // Remove Later
         switchBackupToSwitch = new Trajectory("rightBackupToSwitch", false),
         switchBackupToSwitchReversed = new Trajectory("rightBackupToSwitch", true),
-        sideToSwitch = new Trajectory("rightToRightSwitch", false),
-        sideToScale = new Trajectory("rightToRightScale", false),
-        shortPath = new Trajectory("short", false),
-        shortPathReversed = new Trajectory("short", true);
+        sideToSwitch = new Trajectory("rightToRightSwitch"),
+        sideToScale = new Trajectory("rightToRightScale"),
+        shortPath = new Trajectory("short");
 
     public static void loadTrajectories() {
         // lol you thought
